a90ed7fbc38a8aaec3be4855460c0565a0e0aee7
Silburn <Ben.Silburn@brenntag.com>
2024-02-06 00:54:03 +0100

    Added Narrator audio

--- public/audio.js
+++ public/audio.js
@@ -24,8 +24,14 @@ class AudioManager {
         }
         // Play the new track and set it as the currently playing track
         track.audio.volume = 0;
+        track.currentTime = 0;
+        if (track.type == AudioTypes.MUSIC) {
+            gsap.set(track.audio, { volume:0 });
+            gsap.to(track.audio, { volume: track.volume, duration: this.duration });
+        } else {
+            gsap.set(track.audio, { volume: track.volume });
+        }
         track.play();
-        gsap.to(track.audio, { volume: track.volume, duration: this.duration });
         this.currentlyPlaying[track.type] = track;
     }
     fadeOut(type) {
@@ -46,8 +52,14 @@ class AudioTrack {
         this.name = name;
         this.type = type;
         this.path = path;
-        this.audio = new Audio(path);
+        this.audio = new Audio();
         this.volume = this.audio.volume = volume;   // store a cache of volume so we can fade out and back in
+
+        // From https://stackoverflow.com/questions/11652197/play-multiple-sound-at-the-same-time
+        var src = document.createElement("source");
+        src.type = "audio/mp3";
+        src.src = path;
+        this.audio.appendChild(src);
     }
 
     play() {
@@ -67,15 +79,22 @@ const AudioTypes = {
 }
 
 const musicTracks = {
-    NIGHT: new AudioTrack('NIGHT', AudioTypes.MUSIC, '/audio/spookynight-1.mp3'),
+    NIGHT: new AudioTrack('NIGHT', AudioTypes.MUSIC, '/audio/spookynight-1.mp3', 0.3),
     MORNING: new AudioTrack('DAY', AudioTypes.MUSIC, '/audio/happyday-1.mp3'),
 }
 const effectTracks = {
     WOLFHOWL: new AudioTrack('WOLFHOWL', AudioTypes['EFFECT'], '/audio/wolfhowl-1.mp3'),
-    WOLFATTACK: new AudioTrack('WOLFATTACK', AudioTypes['EFFECT'], '/audio/wolfattack-1.mp3'),
-    VILLAGER: new AudioTrack('VILLAGER', AudioTypes['EFFECT'], '/audio/villager-1.mp3'),
+    WOLFATTACK: new AudioTrack('WOLFATTACK', AudioTypes['EFFECT'], '/audio/wolfattack-1.mp3')
 } 
-// const narratorTracks = {
-//     NIGHT: new AudioTrack('NIGHT', AudioTypes['NARRATOR'], '/audio/narrator-night-1.mp3'),
-//     DAY: new AudioTrack('DAY', AudioTypes['NARRATOR'], '/audio/narrator-day-1.mp3'),
-// }
+const narratorTracks = {
+    WAKEUP: new AudioTrack('WAKEUP', AudioTypes['NARRATOR'], '/audio/narrator-wakeup-1.mp3'),
+    WOLFOPEN: new AudioTrack('WOLFOPEN', AudioTypes['NARRATOR'], '/audio/narrator-wolf-openeyes-1.mp3'),
+    WOLFCLOSE: new AudioTrack('WOLFCLOSE', AudioTypes['NARRATOR'], '/audio/narrator-wolf-closeeyes-1.mp3'),
+    WITCHOPEN: new AudioTrack('WITCHOPEN', AudioTypes['NARRATOR'], '/audio/narrator-witch-openeyes-1.mp3'),
+    WITCHSAVE: new AudioTrack('WITCHSAVE', AudioTypes['NARRATOR'], '/audio/narrator-witch-save-1.mp3'),
+    WITCHCLOSE: new AudioTrack('WITCHCLOSE', AudioTypes['NARRATOR'], '/audio/narrator-witch-closeeyes-1.mp3'),
+    HEALEROPEN: new AudioTrack('HEALEROPEN', AudioTypes['NARRATOR'], '/audio/narrator-healer-openeyes-1.mp3'),
+    HEALERCLOSE: new AudioTrack('HEALERCLOSE', AudioTypes['NARRATOR'], '/audio/narrator-healer-closeeyes-1.mp3'),
+    SEEROPEN: new AudioTrack('SEERHOPEN', AudioTypes['NARRATOR'], '/audio/narrator-seer-openeyes-1.mp3'),
+    SEERCLOSE: new AudioTrack('SEERCLOSE', AudioTypes['NARRATOR'], '/audio/narrator-seer-closeeyes-1.mp3'),
+}

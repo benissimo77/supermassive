332f8e15f6e8de482ebd030ab4f1a24de0541a83
Ben <ben.silburn@gmail.com>
2024-12-08 21:16:47 +0100

    flyInRacetrack needs to be exported

--- src/quiz/dom.quiz.js
+++ src/quiz/dom.quiz.js
@@ -1035,6 +1035,7 @@ export const dom = {
     DOMshowAnswer,
     TLhideAllPanels,
     TLflyPanelLeft,
+    TLflyInRacetrack,
     TLflyOutRacetrack,
     TLendQuestion,
     TLarrangePlayersInPanelHorizontal,

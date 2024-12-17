b824fbfc58f5c44bc7bdbb12f54afabccb1f3432
Ben <ben.silburn@gmail.com>
2024-12-17 19:44:40 +0100

    Fucking hell...

--- server/room.js
+++ server/room.js
@@ -42,7 +42,7 @@ class Room {
 				// each re-connection will result in a new socketid
 				player.socketid = socket.id;
 			} else {
-				console.log('New player...');
+				console.log('New player...', userObj, this.players);
 				player = new Player(userObj);
 				this.players.push(player);
 			}

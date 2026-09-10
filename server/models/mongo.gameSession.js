d1ab64c2cbf78ff1fdb593a95049a9ef6e67ae8f
Ben <ben.silburn@gmail.com>
2026-09-10 22:06:59 +0200

    Store seasonID to filter out sessions that dont have a leaderboard

--- server/models/mongo.gameSession.js
+++ server/models/mongo.gameSession.js
@@ -8,11 +8,14 @@ const gameSessionSchema = new mongoose.Schema({
     startTime: { type: Date, required: true },
     duration: { type: Number }, // Duration in seconds
 
-    // Competition tracking
+    // Competition tracking:
+    // isLive - if not then we likely won't want to include in leaderboards
+    // verificationLevel allows us to filter out unverified or low-quality sessions from leaderboards
     isLive: { type: Boolean, default: false },
     verificationLevel: { type: Number, default: 3 }, // 0: VideoSwipe, 1: Trusted, 2: Verified, 3: Everyone
     
     // Optional season this game belongs to (null for standalone one-off quizzes)
+    // Current thinking is that a quiz must be placed into a season to be eligible for leaderboard aggregation, but this could change in the future
     seasonID: { type: mongoose.Schema.Types.ObjectId, ref: 'Season', index: true, default: null },
     
     // Flexible metadata for game-specific info (e.g. quiz title, total questions)
